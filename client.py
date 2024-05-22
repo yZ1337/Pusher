44e102ba2eebdf0c7bb0c4893d3dff791568f374
yZ1337 <32521997+yZ1337@users.noreply.github.com>
2024-05-22 10:17:05 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,10 +1,10 @@
 import pusher
 
 pusher_client = pusher.Pusher(
-  app_id=u'1806190',
-  key=u'b466625a5dad96340d2a',
-  secret=u'36bbb1cd7506c37d7e69',
-  cluster=u'eu'
+  app_id=u'APP_ID',
+  key=u'APP_KEY',
+  secret=u'SECRET_KEY',
+  cluster=u'CLUSTER'
 )
 
-pusher_client.trigger(u'my-channel', u'my-event', {u'message': u'Jelle'})
+pusher_client.trigger(u'my-channel', u'my-event', {u'message': u'Test message'})
